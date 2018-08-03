c3f72a443d8b112ce3b2d19fb4c1b55edc7fc6af
roiik16 <roik1234@gmail.com>
2018-08-03 13:58:15 +0200

    UI functionalities + Vuforia core samples
    Added first screen UI and vuforia functionalities. WITHOUT ANY DESIGNS YET.

---
+++ Assets/Scripts/Start_AR.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Start_AR : MonoBehaviour {
+
+    public Button m_YourFirstButton;
+    public GameObject tur;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Button StartGame = m_YourFirstButton.GetComponent<Button>();
+        StartGame.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("Game started");
+        tur.SetActive(false);
+    }
+}
