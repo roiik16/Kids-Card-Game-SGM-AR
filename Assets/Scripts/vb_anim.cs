0cc4077df48c93037bc7f4ada1ca083084d16303
ligrima <lianagrima@gmail.com>
2018-08-09 13:31:11 +0200

    3D and Image database added
    --MAJOR UPDATE--
    The first two 3D assets added
    
    Scripts for virtual buttons added
    Script for audio and animation added

---
+++ Assets/Scripts/vb_anim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject EnableTally;
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("LacieBtn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        cubeAni.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        EnableTally.SetActive(true);
+        cubeAni.Play("OneTallyAnim");
+        Debug.Log("Tally Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("None");
+        Debug.Log("Tally Button released");
+    }
+}
